d062e6b6b97310e35d0368b896b49a94d3cf9373
VINHNT <vinh.ngtienmdb@gmail.com>
2026-06-27 09:19:44 +0700

    style(layout): expand workspace width to full screen

--- src/App.tsx
+++ src/App.tsx
@@ -291,7 +291,7 @@ function AppLayout() {
   <div className="flex-1 flex flex-col min-w-0">
   <Header />
   <main className="flex-1 overflow-y-auto p-4 md:p-6 custom-scrollbar">
-  <div className="max-w-7xl mx-auto h-full col-span-12">
+  <div className="w-full mx-auto h-full col-span-12">
   <ErrorBoundary>
   <Suspense fallback={<LoadingScreen />}>
             <Routes>
